1a5e3bc381f9ca1dadc798501c7e6afc434339c1
Justin Suess <justinsuess@sbcglobal.net>
2019-07-07 20:01:26 -0500

    testing database stuff

--- login.php
+++ login.php
@@ -3,7 +3,9 @@ $connection = mysqli_connect('localhost', 'php', 'default');
 if (!$connection){
     die("Database Connection Failed" . mysqli_error($connection));
 }
-$select_db = mysqli_select_db($connection, 'passwords');
+$select_db = mysqli_select_db($connection, 'booklocker');
+//If selecting the database fails, create it
 if (!$select_db){
-    die("Database Selection Failed" . mysqli_error($connection));
+    $connection->query("CREATE DATABASE booklocker");
 }
+$connection->query("CREATE TABLE users (id int AUTOINCREMENT PRIMARY KEY NOT NULL, username varchar(25), passwordhash VARCHAR(60)) IF NOT EXISTS");
