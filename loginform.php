52751dfcf42b1ac3a449c67427a92869a500d51a
Justin Suess <justinsuess@sbcglobal.net>
2019-07-07 19:48:26 -0500

    login preliminary

---
+++ loginform.php
@@ -0,0 +1,33 @@
+<html>
+	<head>
+		<title>Booklocker Login</title>
+	</head>
+	<body>
+		<center>
+			<h1>Booklocker</h1>
+			<p1>MOTD goes here</p1>
+            <form id="login-form" method="post" action="authen_login.php" >
+                <table border="0.5" >
+                    <tr>
+                        <td><label for="user_id">User Name</label></td>
+                        <td><input type="text" name="user_id" id="user_id"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="user_pass">Password</label></td>
+                        <td><input type="password" name="user_pass" id="user_pass"></input></td>
+                    </tr>
+
+                    <tr>
+
+                        <td><input type="submit" value="Submit" />
+                        <td><input type="reset" value="Reset"/>
+
+                    </tr>
+                </table>
+            </form>
+
+        </center>
+	</body>
+</html>
+
+	
